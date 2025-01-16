63810179335683037baabc2a7ad9c3385d9cb44f
Artem Dimoglov <adimoglov@gmail.com>
2025-01-16 16:24:23 +0200

    amen

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,90 @@
 import React from "react";
 import "./App.css";
-import { TonConnectUIProvider } from "@tonconnect/ui-react";
-import Header from "./components/Header/Header";
-import Main from "./components/Main/Main";
+import {useTonConnectUI} from "@tonconnect/ui-react";
+import {useCallback, useState, useEffect} from "react";
+
 function App() {
+    const [tonConnectUI] = useTonConnectUI()
+    const [tonWalletAddress, setTonWalletAddress] = useState<string | null>(null)
+    const [isLoading,setIsLoading] = useState(true)
+
+    const handleWalletConnection = useCallback((address:string) => {
+        setTonWalletAddress(address)
+        console.log("suckssfully")
+        setIsLoading(false)
+    }, [])
+    const handleWalletDisconnect = useCallback(() => {
+        setTonWalletAddress(null)
+        console.log("sucks")
+        setIsLoading(false)
+    }, [])
+
+    useEffect(() => {
+        const checkConnection = async () => {
+            if (tonConnectUI.account?.address) {
+                handleWalletConnection(tonConnectUI.account?.address)
+            } else {
+                handleWalletDisconnect()
+            }
+        }
+        checkConnection()
+
+        const unsub = tonConnectUI.onStatusChange((wallet) => {
+            if (wallet) {
+                handleWalletConnection(wallet.account.address)
+            } else {
+                handleWalletDisconnect()
+            }
+        })
+
+        return () => {
+            unsub()
+        }
+
+    }, [tonConnectUI, handleWalletConnection, handleWalletDisconnect]);
+
+    const handleWalletAction = async () => {
+        if (tonConnectUI.connected) {
+            setIsLoading(true)
+            await tonConnectUI.disconnect()
+        } else {
+            await tonConnectUI.openModal()
+        }
+    }
+
+    const formatAddress = (address: string) => {
+        // @ts-ignore
+        const tempAddress = address.parse(address).toString()
+        return `${tempAddress.slice(0, 4)}...${tempAddress.slice(-4)}`
+    }
+
+    if (isLoading) {
+        return (
+            <main className='flex min-h-screen flex-col items-center justify-center'>
+                <div>
+                    Loading...
+                </div>
+            </main>
+        )
+    }
   return (
-    <TonConnectUIProvider manifestUrl="https://pressfffff.vercel.app/manifest.json">
-      <Header />
-      <Main />
-    </TonConnectUIProvider>
+        <main className='flex min-h-screen flex-col items-center justify-center'>
+            <h1 className='text-4xl mb-6'>TestAssigmentJS</h1>
+            {tonWalletAddress ? (
+                <div className='flex flex-col items-center gap-y-4'>
+                    <p>
+                        Connected: {formatAddress(tonWalletAddress)}
+                    </p>
+                    <button onClick={handleWalletAction} className='bg-yellow-500 hover:bg-yellow-200 py-2 px-4 rounded-3xl'>
+                        Disconnect
+                    </button>
+                </div>
+            ) : (
+                <button onClick={handleWalletAction} className='bg-yellow-500 hover:bg-yellow-200 py-2 px-4 rounded-3xl'>
+                    Connect TonWallet
+                </button>
+            )}
+        </main>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
+
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
+    <TonConnectUIProvider manifestUrl="https://pressfffff.vercel.app/manifest.json">
+
   <React.StrictMode>
     <App />
   </React.StrictMode>
+    </TonConnectUIProvider>
+
 );

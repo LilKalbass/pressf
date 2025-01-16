aca4b88bad6bee58f01c0ee85f8f938196d11d2b
Artem Dimoglov <adimoglov@gmail.com>
2025-01-16 16:34:48 +0200

    amen

--- src/components/Button/Button.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Button.css";
-
-const Button = (props: any) => {
-  return <button {...props} className={"button " + props.className}></button>;
-};
-
-export default Button;

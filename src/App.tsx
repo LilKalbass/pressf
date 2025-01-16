07faa89de0d0cc2449e9a12840440b51ccdcac8a
Artem Dimoglov <adimoglov@gmail.com>
2025-01-16 15:18:31 +0200

    xf

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 function App() {
   return (
-    <TonConnectUIProvider manifestUrl="https://react-tg-app.web.app/manifest.json">
+    <TonConnectUIProvider manifestUrl="https://pressfffff.vercel.app/manifest.json">
       <Header />
       <Main />
     </TonConnectUIProvider>

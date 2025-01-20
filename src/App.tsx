b553bd05dbd6c6f0a51c552b682372022fbcbd7c
Artem Dimoglov <adimoglov@gmail.com>
2025-01-20 14:30:56 +0200

    try

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 import {Header} from "./components/Header";
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import {HomePage} from "./components/HomePage";
 import {Wallet} from "./components/Wallet";
 
